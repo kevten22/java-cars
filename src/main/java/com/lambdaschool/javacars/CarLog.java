20a992a29e9523ac508f585f8398406676b655cc
Kevin Tena <kevten09@gmail.com>
2019-02-15 10:24:23 -0600

    Added log for Car

---
+++ src/main/java/com/lambdaschool/javacars/CarLog.java
@@ -0,0 +1,19 @@
+package com.lambdaschool.javacars;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CarLog implements Serializable {
+    private final String text;
+    private final String formattedDate;
+
+    public CarLog(String text){
+        this.text = text;
+        Date date = new Date();
+        String strDateFormat = "yyyy-MM-dd hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat();
+        formattedDate = dateFormat.format(date);
+    }
+}
